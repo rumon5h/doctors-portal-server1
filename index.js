d159317bfaa1563897f8df8936e07447627241bb
rumon5 <mdrumonkhan48519@gmail.com>
2022-10-01 09:20:04 +0600

    Get services API implementation done

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.local || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 app.use(cors())
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2cqrt9g.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+        client.connect();
+        const serviceCollection = client.db("doctors_portal").collection("services");
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+
+
+}
+
+run().catch(console.dir());
 
 app.get('/', (req, res) => {
     res.send('Hello Doctors portal');
